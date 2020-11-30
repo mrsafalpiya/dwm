ac51cb68904a2b34e83fecb8f75790cb143f7dc1
Safal Piya <piya.safal@yahoo.com>
2020-11-30 17:10:19 +0545

    Changed font to Source Code Pro

--- config.def.h
+++ config.def.h
@@ -10,8 +10,8 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:pixelsize=14", "JoyPixels:pixelsize=14", "Symbols Nerd Font:pixelsize=14" };
-static const char dmenufont[]       = "JetBrains Mono:pixelsize=14";
+static const char *fonts[]          = { "Source Code Pro:pixelsize=14", "JoyPixels:pixelsize=14", "Symbols Nerd Font:pixelsize=14" };
+static const char dmenufont[]       = "Source Code Pro:pixelsize=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
